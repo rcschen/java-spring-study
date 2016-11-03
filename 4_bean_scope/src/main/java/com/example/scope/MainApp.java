b00357102d1727ed01e486ce9ba9aa5a077f55d9
rcschen <chungshiuan@gmail.com>
2016-11-03 16:00:00 +0800

    4_scope complete

--- 4_bean_scope/src/main/java/com/example/scope/MainApp.java
+++ 4_bean_scope/src/main/java/com/example/scope/MainApp.java
@@ -5,13 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
+      //ApplicationContext context = new ClassPathXmlApplicationContext("Beans-singlton.xml");
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans-prototype.xml");
       HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-
+      objA.getMessage();
       objA.setMessage("I'm object A");
       objA.getMessage();
-
+      
+      System.out.println("it's B's turn");
       HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
       objB.getMessage();
    }
